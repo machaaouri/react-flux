4107ead2d64e0cac33eb7a47d175f8c4cdfac634
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-12-11 13:00:35 +0100

    install bootstrap and jquery

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
+$ = jQuery = require('jquery'); //different ways to reference jQuery, by the dollar sign and by the variable jquery (setting both of those..)
+
 var App = console.log('Hello world from Browserify');
 module.exports = App;
