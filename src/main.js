fa7ec39f08b941a4996abc074382d25d100b6333
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-12-11 17:17:44 +0100

    add home and about components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 $ = jQuery = require('jquery'); //different ways to reference jQuery, by the dollar sign and by the variable jquery (setting both of those..)
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
-React.render(<Home />, document.getElementById('app'));
+(function(win){
+    "use strict";
+
+    var App = React.createClass({
+        render: function(){
+            var Child;
+            switch(this.props.route){
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+
+            return (
+                <div>
+                    <Header/>
+                    <Child/>
+                </div>
+            );
+        }
+    });
+
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(<App route={route} />, document.getElementById('app'));
+    }
+
+    win.addEventListener('hashchange', render);
+    render();
+}(window));
